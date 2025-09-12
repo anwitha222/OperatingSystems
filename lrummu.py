61b3380e5a38db6abf64c401f586f9ac0e30ad54
Anwitha Aromel <anwi.aromel@gmail.com>
2025-09-12 14:33:04 +0930

    test

--- lrummu.py
+++ lrummu.py
@@ -1,34 +1,120 @@
 from mmu import MMU
+from collections import OrderedDict
 
 class LruMMU(MMU):
     def __init__(self, frames):
         # TODO: Constructor logic for LruMMU
-        pass
+        #configure
+        self.frames = frames
+        #debug flag
+        self.debug = False
+
+        #counters set up
+        self.disk_reads = 0
+        self.disk_writes = 0
+        self.page_faults = 0
+
+        #frame table
+        self.frame_table = [None] * frames
+
+        #fast lookup: page -> frame index
+        self.page_to_frame = {}
+
+        #free frames to use before any eviction
+        self.free_frames = list(range(frames))
+
+        #least recently used order: keys are pages, most recently used at the end
+        self.lru = OrderedDict()
+
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
-        pass
+        self.debug = True
 
     def reset_debug(self):
         # TODO: Implement the method to reset debug mode
-        pass
+        self.debug = False
 
     def read_memory(self, page_number):
         # TODO: Implement the method to read memory
-        pass
+        self._access(page_number, is_write = False)
+
 
     def write_memory(self, page_number):
         # TODO: Implement the method to write memory
-        pass
+        self._access(page_number, is_write = True)
 
     def get_total_disk_reads(self):
         # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
         # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
         # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
+    
+    #helpers
+
+    def _access(self, page_number, is_write):
+        #hit
+        if page_number in self.page_to_frame:
+            frame = self.page_to_frame[page_number]
+            slot = self.frame_table[frame]
+
+            #mark dirty on write
+            if is_write and not slot ["dirty"]:
+                slot["dirty"] = True
+
+            #refresh lru: move to mru
+            if page_number in self.lru:
+                self.lru.move_to_end(page_number, last = True)
+            else:
+                self.lru[page_number] = True
+
+            if self.debug: 
+                op = "W" if is_write else "R"
+                print(f"hit: page {page_number} in frame {frame} ({op}) dirty = {slot['dirty']}")
+            return
+        #miss
+        self.page_faults += 1
+        self.disk_eads += 1
+
+        #use free frame is there is one
+        if self.free_frames: 
+            frame = self.free_frames.pop(0)
+            self._install_page(frame, page_number, is_write)
+            if self.debug:
+                op = "W" if is_write else "R"
+                print(f"miss: load page {page_number} into free frame {frame} ({op})")
+            return 
+        
+        #evict lru at the front of ordered dict
+        victim_page, _ = self.lru.popitem(last = False)
+        victim_frame = self.page_to_frame.pop(victim_page)
+        victim_slot = self.frame_table[victim_frame]
+
+        if victim_slot["dirty"]:
+            self.disk_writes += 1
+            if self.debug:
+                print(f"evict: page {victim_page} from frame {victim_frame} (dirty->write back)")
+
+        #install new page in empty frame
+        self._install_page(victim_frame, page_number, is_write)
+        if self.debug: 
+            op = "W" if is_write else "R"
+            print(f"miss: load page {page_number} into frame {victim_frame} ({op})")
+
+    def _install_pages(self, frame, page_number, is_write):
+        #place page in frame and update the structures
+        self.frame_table[frame] = {
+            "page": page_number, 
+            "dirty": bool(is_write),
+        }
+
+        self.page_to_frame[page_number] = frame
+        self.lru[page_number] = True
+        self.lru.move_to_end(page_number, last=True) #set as most recently used 
+
